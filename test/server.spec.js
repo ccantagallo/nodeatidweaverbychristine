29386fabb6f77ba99528c1abac7be73e15d4e499
ccantagallo <christine.cantagallo@idweaver.com>
2016-02-18 12:16:17 +0000

    first test

---
+++ test/server.spec.js
@@ -0,0 +1,13 @@
+var mocha = require("mocha");
+var chai = require("chai");
+
+var expect = chai.expect;
+
+describe("when I run my first test", function(){
+    it("should expect true",function(){
+        
+        expect(true).equal(true);
+    });
+    
+    
+});
