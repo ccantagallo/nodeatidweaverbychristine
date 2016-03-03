b1b697cba49803e89ab38bcf3581538f1a32e54d
ccantagallo <christine.cantagallo@idweaver.com>
2016-03-03 11:48:11 +0000

    test image

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 
 var app = express();
 
+app.user(express.static)('public'));
+
 app.get("/", function(req,res,next)
 {
     res.send("Node at idweaver by Christine");
